b7962e9336adb4f1ffe6a3f020d425a34137f15b
Bogdan <shmatovb@mail.ru>
2021-03-08 15:39:43 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Items.AddRange(new object[] {
+            "Нам нужен только манекен с параметрами Энни.",
+            "Каждому человеку, которому ты даришь доверие, ты даешь в руки нож.",
+            "Она не сказала ни слова, но переполненный любовью",
+            "Люди ведь не всегда говорят, что думают.",
+            "When you assume negative intent, you\'re angry.",
+            "Natural Selection is Anything But Random.",
+            "We can all fight against loneliness by engaging in random Acts of Kindness."});
+            this.listBox1.Location = new System.Drawing.Point(35, 25);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(904, 301);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 517);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START TO LOWER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(643, 528);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ".";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 348);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(712, 108);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(770, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 108);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(864, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 108);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.button4.Location = new System.Drawing.Point(952, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 301);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(226, 518);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(174, 52);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "START TO UPPER";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(417, 518);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(174, 52);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "ONLY TITLE";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(30, 467);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 27);
+            this.label2.TabIndex = 9;
+            this.label2.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 581);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Bogdan Shmatov ListBOX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { 
+                // Получаем номер выделенной строки
+                int index = listBox1.SelectedIndex;
+                // Считываем строку в перменную str
+                string str = (string)listBox1.Items[index];
+                var result = CultureInfo.InvariantCulture.TextInfo.ToLower(str);
+                listBox1.Items.RemoveAt(index);
+                listBox1.Items.Insert(index, result);
+                Console.WriteLine(result);
+                // Узнаем количество символов в строке
+                int len = str.Length;
+                // Считаем, что количество пробелов равно 0
+                int count = 0;
+                // Устанавливаем счетчик символов в 0
+                int i = 0;
+                //Организуем цикл перебора всех символов в строке
+                while (i < len - 1)
+                {
+                    // Если нашли пробел, то увеличиваем
+                    // счетчик пробелов на 1
+                    if (str[i] == ' ')
+                        count++;
+                    i++;
+                }
+                label1.Text = "Количество пробелов = " +
+                count.ToString();
+                label2.Text = result;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                label1.Text = "Для продолжения выберете строку!";
+                label2.Text = "Элемент не выбран!";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+                int index = listBox1.SelectedIndex;
+                // Считываем строку в перменную str
+                string str = (string)listBox1.Items[index];
+                var result = CultureInfo.InvariantCulture.TextInfo.ToUpper(str);
+                listBox1.Items.RemoveAt(index);
+                listBox1.Items.Insert(index, result);
+                label2.Text = result;
+            } catch (ArgumentOutOfRangeException)
+            {
+                label1.Text = "Для продолжения выберете строку!";
+                label2.Text = "Элемент не выбран!";
+            }
+}
+
+        private void button6_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                int index = listBox1.SelectedIndex;
+                // Считываем строку в перменную str
+                string str = (string)listBox1.Items[index];
+                var result1 = CultureInfo.InvariantCulture.TextInfo.ToLower(str);
+                var result = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(result1);
+                listBox1.Items.RemoveAt(index);
+                listBox1.Items.Insert(index, result);
+                label2.Text = result;
+
+            } catch (ArgumentOutOfRangeException)
+            {
+                label1.Text = "Для продолжения выберете строку!";
+                label2.Text = "Элемент не выбран!";
+            }
+        }
+    }
+}
